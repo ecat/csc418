2e0f9ce3423c1a93de8d96e01f2459fc2348fb3c
ecat <pkl.plum@gmail.com>
2015-11-13 21:51:23 -0500

    Added intersection with unit square

--- raytracer/scene_object.cpp
+++ raytracer/scene_object.cpp
@@ -26,6 +26,41 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
 
+	// Convert ray to object space
+	Ray3D r_objectSpace(worldToModel * ray.origin, worldToModel * ray.dir);
+
+	// Determine intersection of unit square with ray 
+	double t = - (r_objectSpace.origin[2])/(r_objectSpace.dir[2]);
+
+	// Intersection is behind eye
+	if(t <= 0)
+		return false;
+
+	// Check that x and y values lie within unit square
+	double x_check = r_objectSpace.origin[0] + t * r_objectSpace.dir[0];
+	double y_check = r_objectSpace.origin[1] + t * r_objectSpace.dir[1];
+
+	if(abs(x_check) < 0.5 && abs(y_check) < 0.5){
+		// Intersection lies within unit square
+
+		// Check that there was no previous intersection and that the intersection is nearest
+		// the eye
+		if(ray.intersection.none || t < ray.intersection.t_value){
+			// New intersection is good so update the intersection values
+			ray.intersection.t_value = t;
+
+			// Bring the points and normals back to world space
+			ray.intersection.point = modelToWorld * Point3D(x_check, y_check, 0);
+
+			// Bringing normals back to world space requires special math
+			ray.intersection.normal = worldToModel.transpose() * Vector3D(0, 0, 1);
+
+			ray.intersection.none = false;
+
+			return true;
+		}
+	}
+
 	return false;
 }
 
